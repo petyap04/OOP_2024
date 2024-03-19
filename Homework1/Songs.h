99964cefaf599a95f3e8d6162873be7730331f26
Petya Petrova <147195169+petyap04@users.noreply.github.com>
2024-03-20 01:00:03 +0200

    Create Songs.h

---
+++ Homework1/Songs.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+constexpr int MAX_SIZE = 64;
+constexpr int SIZE_OF_CONTENT = 256;
+constexpr int MAX_SONGS_IN_PLAYLIST = 30;
+constexpr int COUNT_OF_GENRES = 5;
+
+constexpr unsigned SECONDS_IN_HOURS = 3600;
+constexpr unsigned SECONDS_IN_MINUTE = 60;
+constexpr unsigned DAY_SECONDS = 24 * 3600;
+
+
+class Time {
+    unsigned secondsFromMidnight;
+    bool validateAndSet(unsigned lowerBound, unsigned upperBound, unsigned newValue, unsigned oldVlaue, unsigned multiplier)
+    {
+
+        if (newValue >= lowerBound && newValue <= upperBound)
+        {
+            (secondsFromMidnight -= (oldVlaue * multiplier)) += (newValue * multiplier);
+            return true;
+        }
+        else
+            return false;
+    }
+public:
+    Time() = default;
+    Time(unsigned hours, unsigned mins, unsigned seconds);
+    unsigned getWholeInSec()const;
+    unsigned getHours() const;
+    unsigned getMins() const;
+    unsigned getSeconds() const;
+    bool setHours(unsigned hours);
+    bool setMins(unsigned mins);
+    bool setSeconds(unsigned seconds);
+    void printTime(unsigned time) const;
+    void printTime()const;
+};
+
+
+class Song {
+    char name[MAX_SIZE] = "";
+    Time duration;
+    char genre;
+    char content[SIZE_OF_CONTENT] = "";
+public:
+    Song() = default;
+    Song(const char* name, const Time& t, const char* genre, const char* file) {
+        setName(name);
+        setDuration(t);
+        setGenre(genre);
+        setContent(file);
+    }
+    bool setName(const char* name);
+    void setDuration(const Time& t);
+    bool setGenre(const char* genre);
+    bool setContent(const char* file);
+
+    const char* getName()const;
+    Time getDurationInSeconds()const;
+    char getGenre()const;
+    const char* getContent()const;
+
+    void changeEveryKBit(int k);
+    void printGenre()const;
+    void printSong() const;
+};
+
+class Playlist {
+    Song songsArr[MAX_SONGS_IN_PLAYLIST];
+    size_t countOfSongsInThePlaylist = 0;
+public:
+    Playlist() = default;
+    Playlist(const Song* songsArr, size_t countOfSongs) {
+        setSongsArr(songsArr, countOfSongs);
+    }
+    bool setSongsArr(const Song* songsArr, size_t countOfSongs);
+    size_t getCountOfSongs()const;
+    Song findSong(const char* name) const;
+    Song* findByGenre(char genre) const;
+    void add(const Song& song);
+    void printPlaylist()const;
+    void sortByDuration();
+    void sortByName();
+}; 
