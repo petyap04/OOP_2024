cdcf4b6ca8b4f3329a70a6b85f14a9bde56b6fad
Petya Petrova <147195169+petyap04@users.noreply.github.com>
2024-03-24 16:15:33 +0200

    Update HTML tables parser.h

--- Homework1/HTML tables parser.h
+++ Homework1/HTML tables parser.h
@@ -9,6 +9,7 @@ struct Row {
     Field row[MAX_FIELDS_IN_ROW] = {};
 };
 
+
 class Table {
     size_t countOfColls = 0;
     size_t countOfRows = 0;
@@ -16,13 +17,20 @@ class Table {
 public:
     Table();
     Table(size_t countOfColls, size_t countOfRows, const Row* rows);
+
     void swapTwoRows(int index1, int index2);
+
     void writeRowInTable(Row& r, const Row& rowToAdd, size_t size);
+
     void add(size_t rowNumber, const Row& rowToAdd);
+
     void remove(size_t rowNumber);
+
     void editFieldInRow(Row& row, int colNumber, const Field& f);
+
     void edit(int rowNumber, int colNumber, const Field& f);
-    void print()const ;
+
+    void print()const;
 
     size_t getCountOfColls() const;
     size_t getCountOfRows() const;
@@ -30,5 +38,6 @@ public:
 
     bool setCountOfColls(size_t colls);
     bool setCountOfRows(size_t rows);
+    void setFieldInRow(char str, size_t indexOfRow, size_t indexOfColl);
     bool setFieldInRow(const char* str, size_t indexOfRow, size_t indexOfColl);
-};
+}; 
