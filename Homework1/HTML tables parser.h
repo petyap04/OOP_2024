db6b7e9cd91c20c3a1859b64064e61c8e232a27d
Petya Petrova <147195169+petyap04@users.noreply.github.com>
2024-03-24 20:34:40 +0200

    Update HTML tables parser.h - Final Version

--- Homework1/HTML tables parser.h
+++ Homework1/HTML tables parser.h
@@ -3,6 +3,11 @@ constexpr int MAX_SIZE_OF_FIELD = 50;
 constexpr int MAX_FIELDS_IN_ROW = 15;
 constexpr int MAX_ROW = 100;
 
+constexpr char DEF_OF_TABLE[] = "table";
+constexpr int MAX_SIZE_OF_TAG = 7;
+constexpr int SIZE_OF_CHARECTER_REFERENCE = 2;
+constexpr char REFERENCE[SIZE_OF_CHARECTER_REFERENCE + 1] = "&#";
+
 typedef char Field[MAX_SIZE_OF_FIELD];
 
 struct Row {
@@ -20,7 +25,7 @@ public:
 
     void swapTwoRows(int index1, int index2);
 
-    void writeRowInTable(Row& r, const Row& rowToAdd, size_t size);
+    void writeRowInTable(const Row& rowToAdd, int indexOfRow, size_t size);
 
     void add(size_t rowNumber, const Row& rowToAdd);
 
@@ -35,6 +40,7 @@ public:
     size_t getCountOfColls() const;
     size_t getCountOfRows() const;
     const Row& getRow(int index) const;
+    const Field& getFieldInRow(int indexOfRow, int indexOfColl) const;
 
     bool setCountOfColls(size_t colls);
     bool setCountOfRows(size_t rows);
